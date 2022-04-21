3643130df119b0189b2cc2df2adfd03a55663ef2
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-21 13:34:29 +0530

    harmonic value

---
+++ BasicProgramsCSharp/Harmonic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramsCSharp
+{
+    public class Harmonic
+    {
+        public void HarmonicSeries()
+        {
+            double H = 0;
+            Console.WriteLine("Enter the values of n");
+            double n = Convert.ToDouble(Console.ReadLine());
+            for (double i = 1; i < (n + 1); i++)
+            {
+                H += 1 / i;
+                Console.Write(" 1/{0}+ ",i);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("The nth Harmonic of n is :"+H);
+        }
+    }
+}
