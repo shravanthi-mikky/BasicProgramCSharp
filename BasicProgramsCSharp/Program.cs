1c149a260018438995aaf3df79919ff512eab077
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-21 10:24:50 +0530

    Leap Year

--- BasicProgramsCSharp/Program.cs
+++ BasicProgramsCSharp/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Do you want to Run the program?");
 bool status = Convert.ToBoolean(Console.ReadLine());
 while (status)
 {
-    Console.WriteLine("Choose the Program to be executed :\n1)Flip Coin");
+    Console.WriteLine("Choose the Program to be executed :\n1)Flip Coin\n2)Hello Message");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -13,6 +13,11 @@ while (status)
             Console.WriteLine("Welcome to Flip Coin Simulator!");
             FLipcoin flipCoin = new();
             flipCoin.FlipCoin();
+            break;
+        case 2:
+            Console.WriteLine("Wemcome to Leap Year Problem!");
+            LeapYear leapYear = new();
+            leapYear.LeapYearFunction();
             break;
         default:
             break;
